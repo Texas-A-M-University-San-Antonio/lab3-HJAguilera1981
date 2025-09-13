47b1fad5469f3d68b8fcb5db2766a3b247456b39
HJAguilera1981 <hagui018@jaguar.tamu.edu>
2025-09-13 21:39:38 +0000

    new changes

--- InClass3_Herman_Aguilera.java
+++ InClass3_Herman_Aguilera.java
@@ -6,18 +6,29 @@
  */
 
 // Don't forget to import and declare a Scanner for user input.
+import java.util.Scanner; //Import the Scanner class
+// Inside main method;
+Scanner input = new Scanner(system.in); // Declare a Scanner object
 
-public class InClass3_FirstName_LastName
+public class InClass3_Herman_Aguilera
 {
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(Sytem.in);
         // In-Class 3 Example:
         // Step 1: Prompt the user to enter the length of the side of a cube.
+        System.ou.print("Enter the length of the side of the cube: ");
         // Step 2: Read the user's input and store it in a variable.
+        double side = inpit.nextDouble();
         // Step 3: Use an appropriate method to determine the volume of the cube.
+        double volume = Math.power(side, 3);
         // Step 4: Use an appropriate method to determine the surface area of the cube.
+        double surfaceArea = 6 * Math.pow(side, 3);
         // HINT: Steps 3 and 4 will use the Math.pow(a, b) method
         // Step 5: Print the volume and surface area in clear sentences.
+        System.out.println("The volume of the cube is: " + volume);
+        System.out.println("The surface area of the cube is: " + surfaceArea);
+
+        input.close(); // Close the scanner to prevent resource leaks
     }
 }
